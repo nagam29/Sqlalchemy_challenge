0dd9d0caa268c1102f1ce2dfb7e5341c9585b981
nagam29 <mika.nagamine@gmail.com>
2020-04-14 08:05:02 -0700

    4/14/2020 update1

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-# SQLAlchemy_Challenge
+# SQLAlchemy_Challenge : 
+
+# Import Flask
+from flask import Flask
+# Import JSON
+from flask import Flask, jsonify
+
+# Create an app
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+# Define what to do when a user hits the index route
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to My HI Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2017-01-01<br/>"
+        f"/api/v1.0/2017-01-10"
+    )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
